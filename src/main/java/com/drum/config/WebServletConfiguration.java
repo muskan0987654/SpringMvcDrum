d2becc2a0e11965606c298c73a7637ccdb29a8b0
muskan0987654 <muskannandwal83801@gmail.com>
2025-04-16 16:13:19 +0530

    update

---
+++ src/main/java/com/drum/config/WebServletConfiguration.java
@@ -0,0 +1,24 @@
+package com.drum.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+
+public class WebServletConfiguration implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(ServletContext ctx) throws ServletException{
+	
+		AnnotationConfigWebApplicationContext anwebConfig = new AnnotationConfigWebApplicationContext();
+		anwebConfig.register(SpringConfig.class);
+		anwebConfig.setServletContext(ctx);
+		
+		ServletRegistration.Dynamic servlet =ctx.addServlet("dispatcher", new DispatcherServlet(anwebConfig));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+		
+	}
+}
